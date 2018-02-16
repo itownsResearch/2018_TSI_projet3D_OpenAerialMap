f71f3d753b11029eb457f99add9d95faa1820078
sadou <diaksadouminthe@gmail.com>
2018-02-16 01:17:05 +0100

    trying to handle click event

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+var container;
+var renderer,camera, projector;
+
+var viewWidth = window.innerWidth;
+var viewHeight = window.innerHeight;
+var cameraOffset = window.innerWidth;
+var objectsLists = [];
 // Position near Gerbier mountain.
+
+
+
 const positionOnGlobe = { longitude: 2.351323, latitude: 48.856712, altitude: 400000};
 
 // `viewerDiv` will contain iTowns' rendering area (`<canvas>`)
 var viewerDiv = document.getElementById('viewerDiv');
-
 // Instanciate iTowns GlobeView*
-var globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe, { renderer: renderer });
+var globeView = new itowns.GlobeView(viewerDiv, positionOnGlobe, renderer);
 
+
+camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+
+
+var camLocation =  globeView.controls.getCameraLocation();
+var xyz = camLocation.as(globeView.referenceCrs).xyz();
+camera.position.x = xyz.x ;
+camera.position.y = xyz.y;
+camera.position.z = xyz.z;
+
+console.log(globeView);
+//globeView.scene.add(camera);
+camera.lookAt(globeView.scene.position);
 var promises = [];
 
 var menuGlobe = new GuiTools('menuDiv');
@@ -34,13 +56,14 @@ function displayOrthos()
   itowns.Fetcher.json('https://api.openaerialmap.org/meta').
   then(data=>
     {
-      for (var i = 0; i < data.results.length; i++) 
+      for (var i = 0; i < data.results.length; i++)
       {
         var bbox = data.results[i].bbox;
         var coords = data.results[i].geojson.coordinates[0];
         var properties = data.results[i].properties;
         addMeshToScene(bbox,coords,properties);
       }
+          render();
     });
 }
 
@@ -50,7 +73,7 @@ function addMeshToScene(bbox,coords,properties) {
     var long1 = bbox[1];
     var lat2 = bbox[2];
     var long2 = bbox[3];
-    
+
     var bboxCoords1 = new itowns.Coordinates('EPSG:4326',lat1 , long1,0);
     var bboxCoords2 = new itowns.Coordinates('EPSG:4326',lat2 , long2,0);
 
@@ -69,7 +92,7 @@ function addMeshToScene(bbox,coords,properties) {
     var material = new THREE.MeshBasicMaterial({ map: texture,side:THREE.DoubleSide });
     //var material = new THREE.MeshBasicMaterial({ color: 0xff0000,side:THREE.DoubleSide });
     var mesh = new THREE.Mesh(geometry, material);
-    
+
     var imCoords = new itowns.Coordinates('EPSG:4326',coords[0][0] , coords[0][1],0);
     // get the position on the globe, from the camera
 
@@ -89,18 +112,52 @@ function addMeshToScene(bbox,coords,properties) {
     // update coordinate of the mesh
     mesh.updateMatrixWorld();
 
-    var img = mesh.material.map.image;
-    img.addEventListener("click",function(){
-      alert("hello")
-    })
-    console.log(mesh.material.map.image);
-    
+
     // add the mesh to the scene
     globeView.scene.add(mesh);
+
+    objectsLists.push(mesh);
+
     globeView.controls.setCameraTargetGeoPosition({longitude:-74.70703125, latitude:19.775390624999996}, true);
-    
+
     // make the object usable from outside of the function
-    globeView.mesh = mesh;
+    //globeView.mesh = mesh;
+}
+
+var mouse = new THREE.Vector2();
+
+function onDocumentMouseDown( event ) {
+
+    //event.preventDefault();
+
+    mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+
+    console.log(mouse.x,mouse.y);
+    var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+    projector.unprojectVector( vector, camera );
+    var ray = new THREE.Raycaster( camera.position, camera.position.sub( vector ).normalize() );
+
+    var intersects = ray.intersectObjects( objectsLists );
+
+    console.log("Camera Position ", camera.position);
+    console.log("Globe camera position", globeView.controls.getCameraLocation());
+    console.log("Globe norm", camera.position.sub( vector ).normalize());
+    console.log("Globe", camera.position.sub( vector ).normalize());
+    console.log("intersects list", intersects );
+    //chromium-browser --disable-web-security --user-data-dir
+
+    // Change color if hit block
+    if ( intersects.length > 0 )
+    {
+        intersects[ 0 ].object.material.color.setHex( Math.random() * 0xffffff );
+    }
+    render();
+}
+
+function render()
+{
+  renderer.render( globeView.scene, camera );
 }
 
 // Listen for globe full initialisation event
@@ -111,8 +168,24 @@ globeView.addEventListener(itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED, function 
         menuGlobe.addImageryLayersGUI(globeView.getLayers(function (l) { return l.type === 'color'; }));
         menuGlobe.addElevationLayersGUI(globeView.getLayers(function (l) { return l.type === 'elevation'; }));
 
-        //addMeshToScene();
+        renderer = new THREE.CanvasRenderer();
+      	renderer.setSize( viewWidth, viewHeight );
+      	renderer.domElement = $("#viewerDiv").children()[0];
+        $("#viewerDiv").children()[0].replaceWith(renderer.domElement );
+        renderer.setClearColor( 0xffffff );
+
+
+      //  renderer.domElement.onclick = function(e){alert("helloe")};
         displayOrthos();
+
+        projector = new THREE.Projector();
+
+
+        document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+
+
+        camera.lookAt(new THREE.Vector3(0,0,0));
+        render();
 
         globeView.controls.setTilt(10, true);
     });
